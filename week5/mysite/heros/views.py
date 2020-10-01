770764c64f88c3e338b282d08dc2e97ac9fb5897
Ashley <ashley.carlson1997@gmail.com>
2020-09-30 21:50:45 -0600

    Week5
    Unfinished but need sleep

---
+++ week5/mysite/heros/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+class GinsburgView(TemplateView):
+    template_name = "ginsburg.html"
+
+class OkoyeView(TemplateView):
+    template_name = "okoye.html"
+
+class ElectraView(TemplateView):
+    template_name = "electra.html"
+
+class HarleyView(TemplateView):
+    template_name = "harley.html"
+
+class SheraView(TemplateView):
+    template_name = "shera.html"
+
+class IndexView(TemplateView):
+    template_name = "index.html"
